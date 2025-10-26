938c2c5dc7720a8e4beec7edb61fad3421091e97
BC <ben.cirrus@me.com>
2025-10-25 17:19:06 -0700

    Initial Commit

---
+++ StretchTimer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StretchTimer Watch App
+//
+//  Created by Ben Cirrus on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
